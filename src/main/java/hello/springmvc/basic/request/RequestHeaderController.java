71c32e2332a4b4ac50f508749c196d3440a4f1e4
lazzzykim <33sunghyeon@gmail.com>
2025-06-27 21:42:12 +0900

    HTTP 요청 - 기본, 헤더조회

--- src/main/java/hello/springmvc/basic/request/RequestHeaderController.java
+++ src/main/java/hello/springmvc/basic/request/RequestHeaderController.java
@@ -1,9 +1,15 @@
 package hello.springmvc.basic.request;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 public class RequestHeaderController {
+
+    @RequestMapping(value = "/hello-basic")
+    public String hello() {
+        return "hello";
+    }
 }
